79573fdc8d654b9cc1ee87ed26d8469e3312a36e
Hel1umJ <brendanisaboss2000@gmail.com>
2022-10-12 15:51:32 -0400

    first time using github staging pogggggggggggg

--- rcCar.ino
+++ rcCar.ino
@@ -1,32 +1,47 @@
+//All motors are off by default. 
+
+
 #include <SoftwareSerial.h>
 
 //bluetooth pins
 int rdx = 3;
 int txd = 2;
 
+//serial communication object
+SoftwareSerial blueTooth(tdx,txd);
+
 //rMotor pins
-int ena = 4;
-int in1 = 5;
-int in2 = 6;
+//ena
+int speedPinA = 4;
+//in1; (clockwise)
+int dirPin1A = 5;
+//in2; (counterClockwise)
+int dirPin2A = 6;
 
 //lMotor pins
-int enb = 8;
-int in4 = 9;
-int in3 = 10;
+//enb
+int speedPinB = 8;
+//in4; (clockwise)
+int dirPin1B = 9;
+//in5; (counterClockwise)
+int dirPin2B = 10;
 
-
+//declare pinmodes, initialize values. Setup: "the constructor of an arduino" (it has the same functionality save for actually creating and returning an object referece).
 void setup(){
+    pinMode(speedPinA, OUTPUT);
+    pinMode(dirPin1A, OUTPUT);
+    pinMode(dirPin2A, OUTPUT);
 
+    pinMode(speedPinB, OUTPUT);
+    pinMode(dirPin1B, OUTPUT);
+    pinMode(dirPin2B, OUTPUT);
 }
 
 void loop(){
 
 }
 
-
-
-
-
+//High level motor control. 
 void turnRight(){
     //lFront and lBack motors +1
     //rFront and rBack motors -1
@@ -43,4 +58,27 @@ void forward(){
 
 void backward(){
     //all motors -1
+}
+
+
+/*
+*
+*Low level motor control functions. 
+*
+*/
+void lMotorsForward(){
+    analogWrite(speedPinA, 255);
+}
+//turns off all motors on the left. 
+void lMotorsStop(){
+
+}
+
+void rMotorsForward(){
+    analogWrite(speedPinA, 255);
+}
+
+//Turns off all motors on the right.
+void rMotorsStop(){
+
 }
