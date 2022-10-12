e7efe73bae84aa68fd8cddd149fd02b9c7d67850
Hel1umJ <brendanisaboss2000@gmail.com>
2022-10-11 22:18:06 -0400

    first commit

---
+++ rcCar.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+
+
+//bluetooth pins
+int rdx = 3;
+int txd = 2;
+
+//rMotor pins
+int ena = 4;
+int in1 = 5;
+int in2 = 6;
+
+//lMotor pins
+int enb = 8;
+int in4 = 9;
+int in3 = 10;
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
+
+
+
+
+
+void turnRight(){
+    //lFront and lBack motors +1
+    //rFront and rBack motors -1
+}
+
+void turnLeft(){
+    //lFront and lBack motors -1
+    //rFront and rBack motors +1
+}
+
+void forward(){
+    //all motors +1
+}
+
+void backward(){
+    //all motors -1
+}
