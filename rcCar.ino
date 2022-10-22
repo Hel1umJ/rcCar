bc3ee8a2eafbed020c43f4b1be3cd18af4a8a325
Hel1umJ <brendanisaboss2000@gmail.com>
2022-10-21 20:25:06 -0400

    Front and Back motor testing.

--- rcCar.ino
+++ rcCar.ino
@@ -1,30 +1,28 @@
-//All motors are off by default. 
-
-
 #include <SoftwareSerial.h>
 
-//bluetooth pins
-int rdx = 3;
-int txd = 2;
+//Default: motors off
 
-//serial communication object
-SoftwareSerial blueTooth(tdx,txd);
+// //bluetooth pins
+// int rdx = 3;
+// int txd = 2;
+// //serial communication object
+// SoftwareSerial blueTooth(tdx,txd);
 
 //rMotor pins
 //ena
-int speedPinA = 4;
+int speedPinA = 2;
 //in1; (clockwise)
-int dirPin1A = 5;
+int dirPin1A = 3;
 //in2; (counterClockwise)
-int dirPin2A = 6;
+int dirPin2A = 4;
 
 //lMotor pins
 //enb
-int speedPinB = 8;
+int speedPinB = 5;
 //in4; (clockwise)
-int dirPin1B = 9;
+int dirPin1B = 6;
 //in5; (counterClockwise)
-int dirPin2B = 10;
+int dirPin2B = 7;
 
 //declare pinmodes, initialize values. Setup: "the constructor of an arduino" (it has the same functionality save for actually creating and returning an object referece).
 void setup(){
@@ -38,25 +36,31 @@ void setup(){
 }
 
 void loop(){
+    motorTestLeft();
+    motorTestRight();
+    int currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
 
 }
 
 //High level motor control. 
-void turnRight(){
+static void turnRight(){
     //lFront and lBack motors +1
     //rFront and rBack motors -1
 }
 
-void turnLeft(){
+static void turnLeft(){
     //lFront and lBack motors -1
     //rFront and rBack motors +1
 }
 
-void forward(){
+static void forward(){
     //all motors +1
 }
 
-void backward(){
+static void reverse(){
     //all motors -1
 }
 
@@ -66,19 +70,70 @@ void backward(){
 *Low level motor control functions. 
 *
 */
-void lMotorsForward(){
+//turns on all motors on left.
+static void lMotorsForward(){
     analogWrite(speedPinA, 255);
-}
-//turns off all motors on the left. 
-void lMotorsStop(){
-
+    digitalWrite(dirPin1A,HIGH);
+    digitalWrite(dirPin2A,LOW);
 }
 
-void rMotorsForward(){
+static void lMotorsReverse(){
     analogWrite(speedPinA, 255);
+    digitalWrite(dirPin1A,LOW);
+    digitalWrite(dirPin2A,HIGH);
 }
 
+//turns off all motors on left. 
+static void lMotorsStop(){
+    analogWrite(speedPinA, 0);
+    digitalWrite(dirPin1A,LOW);
+    digitalWrite(dirPin2A,LOW);
+}
+ 
+static void rMotorsForward(){
+    analogWrite(speedPinB, 255);
+    digitalWrite(dirPin1B,HIGH);
+    digitalWrite(dirPin2B,LOW);
+}
 //Turns off all motors on the right.
-void rMotorsStop(){
-
+static void rMotorsStop(){
+    analogWrite(speedPinB, 0);
+    digitalWrite(dirPin1B,LOW);
+    digitalWrite(dirPin2B,LOW);
 }
+
+
+
+/*
+*Motor testing
+*/
+
+static void motorTestLeft(){
+    unsigned long currentTime = millis();
+    lMotorsForward();
+    while((millis()-currentTime) < 1000){
+        continue;
+    }
+    currentTime = millis();
+    lMotorsReverse();
+    while(millis()-currentTime < 1000){
+        continue;
+    }
+    lMotorsStop();
+}
+
+static void motorTestRight(){
+    unsigned long currentTime = millis();
+    rMotorsForward();
+    while((millis()-currentTime) < 1001){
+        continue;
+    }
+
+    currentTime = millis();
+    rMotorsReverse();
+    while(millis()-currentTime < 1001){
+        continue;
+    }
+    rMotorsStop();
+}
+
