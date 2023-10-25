c77b38d4ad5072a52f6a7285f7696443cb4c50e1
Hel1umJ <81903892+Hel1umJ@users.noreply.github.com>
2023-10-25 18:16:01 -0400

    Update rcCar.ino

--- rcCar.ino
+++ rcCar.ino
@@ -1,46 +1,189 @@
 #include <SoftwareSerial.h>
 
+//Default: motors off
+
 //bluetooth pins
 int rdx = 3;
 int txd = 2;
+//serial communication object
+SoftwareSerial blueTooth(rdx,txd);
 
 //rMotor pins
-int ena = 4;
-int in1 = 5;
-int in2 = 6;
+//ena
+int speedPinA = 2;
+//in1; (clockwise)
+int dirPin1A = 3;
+//in2; (counterClockwise)
+int dirPin2A = 4;
 
 //lMotor pins
-int enb = 8;
-int in4 = 9;
-int in3 = 10;
+//enb
+int speedPinB = 5;
+//in4; (clockwise)
+int dirPin1B = 6;
+//in5; (counterClockwise)
+int dirPin2B = 7;
 
-
+//declare pinmodes, initialize values. Setup: "the constructor of an arduino" (it has the same functionality save for actually creating and returning an object referece).
 void setup(){
+    pinMode(speedPinA, OUTPUT);
+    pinMode(dirPin1A, OUTPUT);
+    pinMode(dirPin2A, OUTPUT);
 
+    pinMode(speedPinB, OUTPUT);
+    pinMode(dirPin1B, OUTPUT);
+    pinMode(dirPin2B, OUTPUT);
 }
 
 void loop(){
 
+
+    /*
+    *Testing
+    */
+    //Testing time variable
+    int currentTimeGlobal = millis();
+    //rMotors F + B + S, lMotors F + B + S
+    //PASSED
+    // motorTestLeft();
+    // motorTestRight();
+    // currentTimeGlobal = millis();
+    // while(millis()-currentTimeGlobal < 1000){
+    //     continue;
+    // }
+    
+    //Forward, stop, turn right, turn left, backwards.
+    forward();
+    currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
+    stop();
+    currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
+    turnRight();
+    currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
+    turnLeft();
+    currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
+    reverse();
+    currentTimeGlobal = millis();
+    while(millis()-currentTimeGlobal < 1000){
+        continue;
+    }
+    stop();
+
+}
+
+//High level motor control. 
+static void turnRight(){
+    lMotorsForward();
+    rMotorsReverse();
+}
+static void turnLeft(){
+    lMotorsReverse();
+    rMotorsForward();
+}
+
+static void forward(){
+    rMotorsForward();
+    lMotorsForward();
+}
+
+static void reverse(){
+    rMotorsReverse();
+    lMotorsReverse();
+}
+
+static void stop(){
+    rMotorsStop();
+    lMotorsStop();
+}
+
+
+/*
+*
+*Low level motor control functions. 
+*
+*/
+//turns on all motors on left.
+static void lMotorsForward(){
+    analogWrite(speedPinA, 255);
+    digitalWrite(dirPin1A,HIGH);
+    digitalWrite(dirPin2A,LOW);
+}
+
+static void lMotorsReverse(){
+    analogWrite(speedPinA, 255);
+    digitalWrite(dirPin1A,LOW);
+    digitalWrite(dirPin2A,HIGH);
+}
+
+//turns off all motors on left. 
+static void lMotorsStop(){
+    analogWrite(speedPinA, 0);
+    digitalWrite(dirPin1A,LOW);
+    digitalWrite(dirPin2A,LOW);
+}
+ 
+static void rMotorsForward(){
+    analogWrite(speedPinB, 255);
+    digitalWrite(dirPin1B,HIGH);
+    digitalWrite(dirPin2B,LOW);
+}
+
+static void rMotorsReverse(){
+    analogWrite(speedPinB, 255);
+    digitalWrite(dirPin1B,LOW);
+    digitalWrite(dirPin2B,HIGH);
+}
+
+//Turns off all motors on the right.
+static void rMotorsStop(){
+    analogWrite(speedPinB, 0);
+    digitalWrite(dirPin1B,LOW);
+    digitalWrite(dirPin2B,LOW);
 }
 
 
 
+/*
+*Motor testing
+*/
 
-
-void turnRight(){
-    //lFront and lBack motors +1
-    //rFront and rBack motors -1
+static void motorTestLeft(){
+    unsigned long currentTime = millis();
+    lMotorsForward();
+    while((millis()-currentTime) < 1000){
+        continue;
+    }
+    currentTime = millis();
+    lMotorsReverse();
+    while(millis()-currentTime < 1000){
+        continue;
+    }
+    lMotorsStop();
 }
 
-void turnLeft(){
-    //lFront and lBack motors -1
-    //rFront and rBack motors +1
+static void motorTestRight(){
+    unsigned long currentTime = millis();
+    rMotorsForward();
+    while((millis()-currentTime) < 1001){
+        continue;
+    }
+
+    currentTime = millis();
+    rMotorsReverse();
+    while(millis()-currentTime < 1001){
+        continue;
+    }
+    rMotorsStop();
 }
 
-void forward(){
-    //all motors +1
-}
-
-void backward(){
-    //all motors -1
-}
